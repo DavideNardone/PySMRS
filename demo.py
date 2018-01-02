4e53bf99773e55b500ba78632dcd9580033f30ff
Davide <davide.nardone@live.it>
2018-01-02 17:43:48 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
 
     print ('Extracting the representatives frames from the video...It may takes a while...')
     smrs = SMRS(data=Y, alpha=5,norm_type=2,
-                thr=10**-8, max_iter=5000, affine=True,
+                thr=[10**-8], max_iter=5000, affine=True,
                 verbose=False,PCA=True)
 
-    rep_ind, C = smrs.smrs()
+    sInd, repInd, C = smrs.smrs()
 
     print ('The representatives are:')
-    print (rep_ind)
+    print (repInd)
