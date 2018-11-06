97c9a67155e92bd1988e2bb951e12433bf47d4d2
Davide <davide.nardone@live.it>
2018-11-06 11:32:43 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,10 +5,10 @@ np.set_printoptions(threshold=np.inf)
 
 if __name__ == '__main__':
 
-    n_feats = 100
-    n_sample = 300
+    n_sample = 500
+    n_feats = 500
 
-    Y = np.random.rand(n_feats,n_sample)
+    Y = np.random.rand(n_sample,n_feats)
 
     print ('Extracting the representatives frames from the video...It may takes a while...')
     smrs = SMRS(data=Y, alpha=5,norm_type=2,
